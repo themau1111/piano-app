1129cdb1ed17c4e4615d116cc2d105364ea9dea0
MAURICIO LOZANO <Mauricio.Lozano@ibm.com>
2025-09-26 11:58:32 -0600

    .

--- src/app/components/ChordExercise.tsx
+++ src/app/components/ChordExercise.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { useEffect, useRef, useState } from "react";
 import * as Tone from "tone";
